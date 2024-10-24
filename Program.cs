ee803f9745102ab4f8af7c5c0754439f6c67caf1
idekferewf <fkwewegklwe@gmail.com>
2024-10-24 08:30:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
