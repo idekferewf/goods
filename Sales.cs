cc84039826497a91e824f65857a64b8dad5fa5cb
idekferewf <fkwewegklwe@gmail.com>
2024-11-05 11:02:35 +0300

    Удалил неиспользуемые импорты

--- Sales.cs
+++ Sales.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace goods
+﻿namespace goods
 {
     public class Sales
     {
